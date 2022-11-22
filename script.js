9412585f8e5f3d52cf87eec51d8696a8f45f7c42
Jacob Simmons <jakerstx@gmail.com>
2022-11-22 16:25:03 -0600

    creates reset button which can start another grid. finsihes functionality of hover.

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
 body = document.querySelector('body')
-body.style.cssText = "display: flex; flex-direction: column; height: 960px; width: 960px"
-for(let i = 0; i < 100; i++) {
-    div = document.createElement('div1')
-    
-    div.setAttribute('id', i)
- 
+body.style.cssText = "display: flex; flex-direction: column; height: 960px; width: 960px; margin: auto"
+function setUp() {
+    let gridSize = 0
+    do {
+        gridSize = prompt("Enter Grid Size (0-100): ")
+    } while(gridSize > 100)
 
-    div.style.cssText = "display: flex; flex-grow: 1"
-    for (let j = 0; j < 100; j++) {
-        innerDiv = document.createElement('div')
-    
-
-        innerDiv.setAttribute("id", i + "_" + j)
+    for(let i = 0; i < gridSize; i++) {
+        div = document.createElement('div1')
         
-        innerDiv.style.cssText = "border: solid; padding: 10px; flex-grow: 1"
-
-        div.appendChild(innerDiv)
-    }
-    body.append(div)
-    }
-    function display(e) {
-        const divToChange = document.getElementById(e.target.id)
-        divToChange.style.background= 'red'
+        div.setAttribute('id', i)
     
-        e.stopPropagation()
-    }
-    const divs = document.querySelectorAll('div')
-    divs.forEach(div => div.addEventListener('mouseover', display, {
-            capture: false
-          }))
+
+        div.style.cssText = "display: flex; flex-grow: 1"
+        for (let j = 0; j < gridSize; j++) {
+            innerDiv = document.createElement('div')
+        
+
+            innerDiv.setAttribute("id", i + "_" + j)
+            
+            innerDiv.style.cssText = "border: solid; border-width: 1px; flex-grow: 1"
+
+            div.appendChild(innerDiv)
+        }
+        body.append(div)
+        }
+        function display(e) {
+            const divToChange = document.getElementById(e.target.id)
+            divToChange.style.background= 'blue'
+        
+            e.stopPropagation()
+        }
+        const divs = document.querySelectorAll('div')
+        divs.forEach(div => div.addEventListener('mouseover', display, {
+                capture: false
+            }))
+        }
+        const btn = document.createElement('button')
+        btn.textContent = "Reset game"
+        body.appendChild(btn)
+        btn.addEventListener('click', () => {
+    
+            while(body.firstChild){
+                body.removeChild(body.firstChild);
+            }
+            body.appendChild(btn)
+            setUp()
+        })
+        setUp()
